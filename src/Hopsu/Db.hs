a46b72029c5f120611fe794886e43e34e1d6538f
anobi <nsalakka@gmail.com>
2014-04-01 22:57:06 +0300

    it kinda works, kinda :o

--- src/Hopsu/Db.hs
+++ src/Hopsu/Db.hs
@@ -5,12 +5,15 @@ import Database.HDBC.Sqlite3
 
 geturl :: Connection -> String -> IO (Maybe String)
 geturl c s = handleSqlError $ do  
-    q <- quickQuery' c "SELECT url FROM urls WHERE name = ?" [toSql s]
+    q <- quickQuery' c "SELECT * FROM urls" []
     case q of
-        [x] -> return $ Just $ convUrl x
-        [] -> return $ Just "nope"
-        x -> return $ Just "nope"
+        ([id,n,url]:_) -> return $ Just $ fromSql n ++ " > " ++ fromSql url
+        _ -> return $ Just $ "AAH" ++ show q
 
 convUrl :: [SqlValue] -> String
 convUrl [svId, svUrl] =
-    show "" ++ fromSql svUrl
+    show val
+    where val = case fromSql svUrl of
+            Just x -> x
+            Nothing -> "nope"
+convUrl x = fail $ "a failure is " ++ show x
