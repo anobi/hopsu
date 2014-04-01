acd5ce2fde005184c3adc84f8cdcdf9211267c34
anobi <nsalakka@gmail.com>
2014-04-01 23:10:03 +0300

    go away useless thing

--- src/Hopsu/Db.hs
+++ src/Hopsu/Db.hs
@@ -3,17 +3,9 @@ module Hopsu.Db where
 import Database.HDBC
 import Database.HDBC.Sqlite3
 
-geturl :: Connection -> String -> IO (Maybe String)
+geturl :: Connection -> String -> IO String
 geturl c s = handleSqlError $ do  
     q <- quickQuery' c "SELECT * FROM urls" []
     case q of
-        ([id,n,url]:_) -> return $ Just $ fromSql n ++ " > " ++ fromSql url
-        _ -> return $ Just $ "AAH" ++ show q
-
-convUrl :: [SqlValue] -> String
-convUrl [svId, svUrl] =
-    show val
-    where val = case fromSql svUrl of
-            Just x -> x
-            Nothing -> "nope"
-convUrl x = fail $ "a failure is " ++ show x
+        ([id,n,url]:_) -> return $ fromSql n ++ " > " ++ fromSql url
+        _ -> return ""
